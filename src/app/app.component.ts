15f200041203883dbc8f4e0a0f937b397524457b
Lucas Brum <lucas_trabach@hotmail.com>
2017-09-02 23:12:21 -0300

    Criando DatabaseService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { DatabaseService } from '../providers/shared/database.service';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
@@ -11,12 +13,16 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, database: DatabaseService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      if(database){
+        database.createDatabase();
+      }
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { SQLite } from '@ionic-native/sqlite';
+import { DatabaseService } from '../providers/shared/database.service';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { ListCategoriaPage } from '../pages/categorias/list/list.categoria';
@@ -35,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,
+    DatabaseService
   ]
 })
 export class AppModule {}
